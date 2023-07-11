486c20a10155519066bd37e8744b83e32a3b52a7
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-11 13:49:42 +0200

    added new observable in desk-list.component

--- src/app/pages/reservation-forms-and-data/desk-list/desk-list.component.ts
+++ src/app/pages/reservation-forms-and-data/desk-list/desk-list.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const deskList = new Observable(function subsribe(subscriber) {
+  let value: any = null;
+  while (true) {
+    setTimeout(() => {
+      value = localStorage.getItem('deskList');
+      subscriber.next(value == null ? null : JSON.parse(value));
+    },1000);
+  }
+});
 
 @Component({
   selector: 'app-desk-list',
@@ -7,6 +18,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeskListComponent implements OnInit {
   constructor() {}
+  x: any = deskList.subscribe();
   deskList: any = [
     {
       deskID: 1,
@@ -17,10 +29,13 @@ export class DeskListComponent implements OnInit {
       reservedBy: 'Adam Kowalski',
     },
   ];
-  testList = [1, 2, 3, 4];
+
   ngOnInit() {
     if (localStorage['deskList'] == null)
       localStorage.setItem('deskList', JSON.stringify(this.deskList));
     else this.deskList = JSON.parse(localStorage['deskList']);
+    while (true) {
+      console.log(this.x);
+    }
   }
 }
--- src/app/pages/reservation-forms-and-data/desk-reservation-form/desk-reservation-form.component.ts
+++ src/app/pages/reservation-forms-and-data/desk-reservation-form/desk-reservation-form.component.ts
@@ -5,13 +5,37 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './desk-reservation-form.component.html',
   styleUrls: ['./desk-reservation-form.component.css'],
 })
+
+//TODO HERE
+//
+//change deskList into an observable (do it in other components to) so they dinamically refresh themselves
+//
+//
+//
+//
+//
+//
 export class DeskReservationFormComponent implements OnInit {
   constructor() {}
   reserveObj: any = {
-    deskID: -1,
+    deskID: 1,
     reservedBy: '',
   };
-
+  deskList: any = [];
   ngOnInit() {}
-  reserveDesk() {}
+  reserveDesk() {
+    this.deskList = localStorage.getItem('deskList');
+    if (this.deskList != null) this.deskList = JSON.parse(this.deskList);
+    if (
+      this.deskList.find(
+        (m: any) => m.deskID == this.reserveObj.deskID && m.reservedBy == ''
+      )
+    ) {
+      this.deskList[this.reserveObj.deskID - 1].reservedBy =
+        this.reserveObj.reservedBy;
+      localStorage.setItem('deskList', JSON.stringify(this.deskList));
+    } else {
+      alert('nie można zarezerwować tego stanowiska');
+    }
+  }
 }
