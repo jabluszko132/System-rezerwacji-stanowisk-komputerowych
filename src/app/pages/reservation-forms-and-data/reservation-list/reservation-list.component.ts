af99593ddb7dff18755a1fc4edd469123bd94786
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-18 09:59:27 +0200

    cleaned up code

--- src/app/pages/reservation-forms-and-data/reservation-list/reservation-list.component.ts
+++ src/app/pages/reservation-forms-and-data/reservation-list/reservation-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { LocalstorageDeskListService } from '../localstorage-desk-list.service';
 import { Reservation } from '../reservation';
@@ -8,17 +8,12 @@ import { Reservation } from '../reservation';
   templateUrl: './reservation-list.component.html',
   styleUrls: ['./reservation-list.component.css'],
 })
-export class ReservationListComponent implements OnInit, AfterViewInit {
+export class ReservationListComponent implements OnInit {
   constructor(private service: LocalstorageDeskListService) {}
   reservationList: Observable<any> = this.service.getReservationList();
 
-  ngAfterViewInit() {
-    // this.service.refreshReservationList();
-  }
   ngOnInit() {
     this.reservationList.subscribe(() => {});
-
-    // this.service.refreshReservationList();
   }
 
   deleteReservation(reservation: Reservation): void {
