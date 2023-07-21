8dd6405326d96d62184969d8ea1b9b8a0cd82131
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-21 09:02:24 +0200

    added reports page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Error404Component } from './pages/error404/error404.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { ReportsPageComponent } from './pages/reports-page/reports-page.component';
 import { ReservationPageComponent } from './pages/reservation-page/reservation-page.component';
 
 const routes: Routes = [
@@ -11,6 +12,7 @@ const routes: Routes = [
 
   { path: 'reservation', component: ReservationPageComponent },
 
+  { path: 'reports', component: ReportsPageComponent },
   //--------------error pages-------------------
   { path: '**', component: Error404Component },
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ReservationPageComponent } from './reservation-page/reservation-page.component';
 import { Error404Component } from './error404/error404.component';
 import { ReservationFormsAndDataModule } from './reservation-forms-and-data/reservation-forms-and-data.module';
+import { ReportsPageComponent } from './reports-page/reports-page.component';
 
 @NgModule({
   declarations: [
     MainPageComponent,
     ReservationPageComponent,
     Error404Component,
+    ReportsPageComponent,
   ],
   imports: [CommonModule, ReservationFormsAndDataModule],
   exports: [],
