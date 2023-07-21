39f8ea9e3b3a07c461556f7de7c479b664e7cb01
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-21 08:58:54 +0200

    Made minimal reservation time 1 hour

--- src/app/pages/reservation-forms-and-data/desk-reservation-form/desk-reservation-form.component.ts
+++ src/app/pages/reservation-forms-and-data/desk-reservation-form/desk-reservation-form.component.ts
@@ -21,11 +21,11 @@ export class DeskReservationFormComponent implements OnInit, OnDestroy {
   ) {}
 
   // deskList$ :Observable<any> = this.service.getDeskList();
-  reservationForm = this.fb.group({
+  reservationForm = this.fb.nonNullable.group({
     deskID: [1,Validators.required],
     reservedBy: ['', Validators.required],
     reservationDate: ['', [Validators.pattern('[0-9]{4}-[0-9]{2}-[0-9]{2}'),Validators.required]],
-    startHour: ['',[Validators.required]],
+    startHour: ['',[Validators.required,]],
     endHour: ['',[Validators.required]]
   });
 
@@ -45,8 +45,8 @@ export class DeskReservationFormComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     endSubs$.complete();
   }
+  
   reserveDesk(): void {
-    console.log(this.deskID.value)
     if(this.deskID.errors || this.reservedBy.errors || this.reservationDate.errors)
     {
       alert('Podaj poprawne wartości we wszystkich polach formularza')
@@ -55,7 +55,15 @@ export class DeskReservationFormComponent implements OnInit, OnDestroy {
     if(this.startHour.value as string >= (this.endHour.value as string)) {
       alert('Godzina początkowa nie może być późniejsza niż, ani równa końcowej');
       return;
+    }
+    if(this.startHour.value as string < '06:00:00' || this.startHour.value as string > '20:00:00' || this.endHour.value as string < '06:00:00' || this.endHour.value as string > '20:00:00') {
+      alert('Rezerwacja musi mieścić się w godzinach pracy (6 - 20)');
+      return;
     } 
+    if(parseInt(this.endHour.value[0] as string + this.endHour.value[1] as string) - parseInt(this.startHour.value[0] as string + this.startHour.value[1] as string) < 1) {
+      alert('Minimalna długość rezerwacji to 1 godzina');
+      return;
+    }
     action$.next(this.reservationForm.value as Reservation);
   }
 }
