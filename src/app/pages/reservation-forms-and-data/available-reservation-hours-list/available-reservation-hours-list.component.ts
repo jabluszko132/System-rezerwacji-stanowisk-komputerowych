fc4094f1c53eba2c05baa311aa896526dac2c621
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-21 11:33:51 +0200

    3 files modified

--- src/app/pages/reservation-forms-and-data/available-reservation-hours-list/available-reservation-hours-list.component.ts
+++ src/app/pages/reservation-forms-and-data/available-reservation-hours-list/available-reservation-hours-list.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { filter, Subject, switchMap, takeUntil } from 'rxjs';
 import { LocalstorageDeskListService } from '../localstorage-desk-list.service';
 import { NumberRange } from '../number-range';
+
+const action$: Subject<any> = new Subject<any>
+const endSubs$: Subject<null> = new Subject<null>;
 
 @Component({
   selector: 'app-available-reservation-hours-list',
   templateUrl: './available-reservation-hours-list.component.html',
   styleUrls: ['./available-reservation-hours-list.component.css'],
 })
-export class AvailableReservationHoursListComponent implements OnInit {
-  constructor(private service: LocalstorageDeskListService) {}
+export class AvailableReservationHoursListComponent implements OnInit, OnDestroy {
+  constructor(private service: LocalstorageDeskListService, private fb: FormBuilder) {}
   availableHours: NumberRange[] = [];
 
-  ngOnInit() {}
+  form = this.fb.nonNullable.group({
+    deskID: ['', [Validators.required]],
+    reservationDate: ['',[Validators.required]],
+  })
 
-  getAvailableHours() {
-    // this.service.availableReservationHoursOnDay();
+  deskID = this.form.controls.deskID;
+  reservationDate = this.form.controls.reservationDate;
+
+  ngOnInit() {
+    action$.pipe(filter((m:any)=>m == this.form.value),switchMap((d:any)=>this.service.availableReservationHoursOnDay(d.deskID,d.reservationDate)),takeUntil(endSubs$)).subscribe();
+  }
+
+  ngOnDestroy() {
+    endSubs$.complete();
+  }
+  
+  getAvailableHours(): void {
+    action$.next(this.form.value);
   }
 }
