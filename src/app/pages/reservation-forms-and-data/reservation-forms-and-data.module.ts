3a382edbea7121a9d3676a1cf339bab1348d11f9
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-27 09:09:27 +0200

    cleaned up code

--- src/app/pages/reservation-forms-and-data/reservation-forms-and-data.module.ts
+++ src/app/pages/reservation-forms-and-data/reservation-forms-and-data.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-// import { DeskReservationFormComponent } from './desk-reservation-form/desk-reservation-form.component';
-// import { DeskAdditionFormComponent } from './desk-addition-form/desk-addition-form.component';
-// import { DeskListComponent } from './desk-list/desk-list.component';
 import { LocalstorageDeskListService } from './localstorage-desk-list.service';
 // import { DeskMalfunctionsModule } from './desk-malfunctions/desk-malfunctions.module';
 import { DeskManagementModule } from './desk-management/desk-management.module';
@@ -16,10 +13,6 @@ import { DeskMalfunctionsModule } from './desk-malfunctions/desk-malfunctions.mo
 import { DeskMalfunctionReportListComponent } from './desk-malfunctions/desk-malfunction-report-list/desk-malfunction-report-list.component';
 import { DeskMalfunctionReportFormComponent } from './desk-malfunctions/desk-malfunction-report-form/desk-malfunction-report-form.component';
 import { ReservationHoursSelectComponent } from './desk-reservations/reservation-hours-select/reservation-hours-select.component';
-// import { ReservationListComponent } from './reservation-list/reservation-list.component';
-// import { DeskMalfunctionReportFormComponent } from './desk-malfunction-report-form/desk-malfunction-report-form.component';
-// import { DeskMalfunctionReportListComponent } from './desk-malfunction-report-list/desk-malfunction-report-list.component';
-// import { AvailableReservationHoursListComponent } from './available-reservation-hours-list/available-reservation-hours-list.component';
 
 @NgModule({
   imports: [
@@ -37,10 +30,6 @@ import { ReservationHoursSelectComponent } from './desk-reservations/reservation
     DeskMalfunctionReportListComponent,
     DeskMalfunctionReportFormComponent,
     ReservationHoursSelectComponent,
-    // DeskListComponent,
-    // ReservationListComponent,
-    // DeskMalfunctionReportFormComponent,
-    // DeskMalfunctionReportListComponent,
   ],
   providers: [LocalstorageDeskListService],
 })
