bdaa6b68ce8de0fad62141582de4ec204221f2f8
jabluszko132 <123495481+jabluszko132@users.noreply.github.com>
2023-07-28 12:42:14 +0200

    cleaned up code

--- src/app/pages/reservation-forms-and-data/desk-management/desk-management.module.ts
+++ src/app/pages/reservation-forms-and-data/desk-management/desk-management.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DeskAdditionFormComponent } from './desk-addition-form/desk-addition-form.component';
 import { DeskListComponent } from './desk-list/desk-list.component';
-import { LocalstorageDeskListService } from '../localstorage-desk-list.service';
 import { DeskManagerService } from './desk-manager.service';
 import { FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
   declarations: [DeskAdditionFormComponent, DeskListComponent],
-  providers: [LocalstorageDeskListService, DeskManagerService, Validators],
+  providers: [DeskManagerService, Validators],
   exports: [DeskAdditionFormComponent, DeskListComponent],
 })
 export class DeskManagementModule {}
--- src/app/pages/reservation-forms-and-data/desk-reservations/desk-reservations.module.ts
+++ src/app/pages/reservation-forms-and-data/desk-reservations/desk-reservations.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { DeskReservationFormComponent } from './desk-reservation-form/desk-reservation-form.component';
 import { ReservationListComponent } from './reservation-list/reservation-list.component';
 import { AvailableReservationHoursListComponent } from './available-reservation-hours-list/available-reservation-hours-list.component';
-import { LocalstorageDeskListService } from '../localstorage-desk-list.service';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { ReservatorService } from './reservator.service';
 import { ReservationHoursSelectComponent } from './reservation-hours-select/reservation-hours-select.component';
@@ -16,7 +15,7 @@ import { ReservationHoursSelectComponent } from './reservation-hours-select/rese
     AvailableReservationHoursListComponent,
     ReservationHoursSelectComponent,
   ],
-  providers: [LocalstorageDeskListService, ReservatorService, Validators],
+  providers: [ReservatorService, Validators],
   exports: [
     DeskReservationFormComponent,
     ReservationListComponent,
